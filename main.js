8737ef0c9572b367e11c8893f8c04b5078174639
Afeez G. Lawal <afeezlg@gmail.com>
2020-12-03 23:05:10 +0100

    create article

--- main.js
+++ main.js
@@ -2,9 +2,27 @@ const container = document.querySelector(".container")
 const createButton = document.querySelector('#create-article')
 const loginForm = document.querySelector('.login-form')
 const createArticleForm = document.querySelector('.create-article-form')
+const articleSubmitButton = document.querySelector('#articleSubmit')
 var isAuthenticated = localStorage.getItem('isAuthenticated')
 var authToken = window.localStorage.getItem('authToken')
 console.log(isAuthenticated)
+
+fetch ("https://prj-django-blog.herokuapp.com/api/articles/",
+{headers: {
+    "Content-Type": "application/json",
+}})
+.then(res => res.json())
+.then((posts) => {
+    console.log(posts)
+    posts.forEach(post => {
+        let article = `<article>
+                            <image src = "${post.image}">
+                            <h4>${post.title}</h4>
+                            <p>${post.short_text}...</p>
+                        </article>`
+        container.innerHTML += article;
+    });
+})
 
 createButton.addEventListener( 'click', function(){
     container.innerHTML = ""
@@ -51,19 +69,25 @@ loginButton.addEventListener('click', function(e){
     .catch((error) => alert(error.message))
 })
 
-fetch ("https://prj-django-blog.herokuapp.com/api/articles/",
-{headers: {
-    "Content-Type": "application/json",
-}})
-.then(res => res.json())
-.then((posts) => {
-    console.log(posts)
-    posts.forEach(post => {
-        let article = `<article>
-                            <image src = "${post.image}">
-                            <h4>${post.title}</h4>
-                            <p>${post.short_text}...</p>
-                        </article>`
-        container.innerHTML += article;
-    });
+articleSubmitButton.addEventListener('click', function(e){
+    e.preventDefault()
+    let image = document.querySelector('#image').value
+    let title = document.querySelector('#title').value
+    let articleBody = document.querySelector('#articleBody').value
+
+    fetch("https://prj-django-blog.herokuapp.com/api/articles", {
+        method: "POST",
+        headers: {
+            "Content-Type": "multipart/form-data",
+            "Authentication": `Token ${authToken}`
+        },
+        body: JSON.stringify({
+            "image": image,
+            "title": title,
+            "body": articleBody
+        })
+    })
+    .then(res => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => alert(error.message))
 })
