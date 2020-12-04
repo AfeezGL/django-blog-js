1c826673b4734e23606a2c43c890b96858b4006f
Afeez G. Lawal <afeezlg@gmail.com>
2020-12-04 09:19:36 +0100

    origin header

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ console.log(isAuthenticated)
 fetch ("https://prj-django-blog.herokuapp.com/api/articles/",
 {headers: {
     "Content-Type": "application/json",
+    "Authentication": `Token ${authToken}`,
 }})
 .then(res => res.json())
 .then((posts) => {
@@ -79,7 +80,7 @@ articleSubmitButton.addEventListener('click', function(e){
         method: "POST",
         headers: {
             "Content-Type": "multipart/form-data",
-            "WWW-Authenticate": `Token ${authToken}`,
+            "Authentication": `Token ${authToken}`,
             "Origin": "https://afeezgl.github.io"
         },
         body: JSON.stringify({
